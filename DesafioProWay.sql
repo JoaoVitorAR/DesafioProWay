621926e2ca3e37c21fe223b5e959d96d763f1767
JoaoVitorAR <jvalves1998@outlook.com.br>
2020-10-04 20:54:47 -0300

    finalizado

---
+++ DesafioProWay.sql
@@ -0,0 +1,57 @@
+CREATE DATABASE  IF NOT EXISTS `desafioproway` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `desafioproway`;
+-- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
+--
+-- Host: localhost    Database: desafioproway
+-- ------------------------------------------------------
+-- Server version	8.0.15
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `jogos`
+--
+
+DROP TABLE IF EXISTS `jogos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `jogos` (
+  `idJogo` int(11) NOT NULL AUTO_INCREMENT,
+  `placar` int(11) NOT NULL,
+  `minTemp` int(11) NOT NULL,
+  `maxTemp` int(11) NOT NULL,
+  `minRecorde` int(11) NOT NULL,
+  `maxRecorde` int(11) NOT NULL,
+  PRIMARY KEY (`idJogo`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jogos`
+--
+
+LOCK TABLES `jogos` WRITE;
+/*!40000 ALTER TABLE `jogos` DISABLE KEYS */;
+INSERT INTO `jogos` VALUES (1,12,12,12,0,0),(2,24,12,24,0,1),(3,10,10,24,1,1),(4,24,10,24,1,1);
+/*!40000 ALTER TABLE `jogos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-10-03  0:36:17
---
+++ README.txt
@@ -0,0 +1,6 @@
+Executar programa atraves da classe java "principal.java" dentro do NetBeans. 
+Usando banco de dados MySql 8.0, alterar usuario e senha na classe "conexao.java" dentro do pacote util.
+
+Ao executar o projeto usar barra de menu "jogos" > "tratamento de jogos" e informar o placar dos jogos.
+
+Tratar os jogos de acordo com a necessidade utilizando comando de pesquisar, salvar, editar, excluir.
